2c387a2c2dee9a996b8b07c6b99790050e4c86ce
André Vital <santiago_vital@outlook.com>
2020-06-05 15:57:08 -0500

    Added second Korea trip landing page with sign up form

---
+++ training_korea/2/send-form-product.php
@@ -0,0 +1,91 @@
+﻿<?php
+	$hostname = "localhost";
+    $username = "intracel_mluser";
+    $password = "y3ll0wp4thyellow";
+    $databaseName = "intracel_controlContacto";
+
+    $connect = mysqli_connect($hostname, $username, $password, $databaseName);
+	mysqli_set_charset ($connect, "utf8");
+	date_default_timezone_set('America/Mexico_City');
+
+	$subject = $_POST['contactSubject'];
+
+	$state = $_POST['contactState'];
+	$fname = $_POST['contactFName'];
+	$lname = $_POST['contactLName'];
+	$mail = $_POST['contactEmail'];
+	$phone = $_POST['contactPhone'];
+	$esp = $_POST['contactSpecialty'];
+
+	if(isset($_POST['contactSubscribe'])) $subscribe = 1;
+	else $subscribe = 0;
+
+	$queryMax = "Select MAX(contactos_ID) from contactocorea";
+    $takeMax = mysqli_query($connect, $queryMax);
+    $maxInd = mysqli_fetch_array($takeMax);
+    $maxInd[0] += 1;
+
+    $queryContact = "Insert into contactocorea (contactos_ID, contactos_nombre, contactos_apellido, contactos_correo, contactos_telefono, contactos_estado, contactos_especialidad, contactos_suscrito) values ($maxInd[0],";
+    $queryContact .= '"'.$fname.'","'.$lname.'",';
+    $queryContact .= "'$mail','$phone','$state','$esp',$subscribe)";
+    $resultContact = mysqli_query($connect, $queryContact);
+
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	$headers .= 'From: ' .$fname. ' ' .$lname. ' <' .$mail. '>' . "\r\n";
+
+	$message = "
+	<html>
+	<head>
+	<title>$subject</title>
+	</head>
+	<body>
+	<table width='50%' border='0' align='center' cellpadding='0' cellspacing='0'>
+	<tr>
+	<td colspan='2' align='center' valign='top'><img style=' width: 250px; margin-top: 15px; ' src='https://www.medestore.mx/assets/img/Logo-MedeStore.png'></td>
+	</tr>
+	<tr>
+	<td width='50%' align='right'>&nbsp;</td>
+	<td align='left'>&nbsp;</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Nombre:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$fname." ".$lname."</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Teléfono:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$phone."</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Email:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$mail."</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Especialidad:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$esp."</td>
+	</tr>
+	<tr>
+	<td align='right' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Estado:</strong></td>
+	<td align='left' valign='top' style='border-top:1px solid #dfdfdf; font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 0 7px 5px;'>".$state."</td>
+	</tr>
+	<tr>
+	<td colspan='2' align='center' valign='top' style='font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000; padding:7px 5px 7px 0;'><strong>Enviado el:</strong> ".date('d-m-Y H:i:s')."</td>
+	</tr>
+	<tr>
+	<td width='50%' align='right'>&nbsp;</td>
+	<td align='left'>&nbsp;</td>
+	</tr>
+	<tr>
+	<td width='50%' align='right'>&nbsp;</td>
+	<td align='left'>&nbsp;</td>
+	</tr>
+	</table>
+	</body>
+	</html>
+	";
+
+	$para = 'contacto@medestore.mx';
+
+	if(mail($para, $subject, $message, $headers)) echo'<body><link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet"><div><div><img src="https://www.medestore.mx/assets/img/logo_medestore-color.png" alt="Logo Medestore"></div><h1>Mensaje Enviado</h1><h4>Gracias por ponerte en contacto con nosotros.<br> En breve uno de nuestros especialistas te contactará</h4></div><style>body{margin: 0;padding: 0;background-color: #fff;background-size: cover;height: 100vh;width: 100vw;}body>div {position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);display: block;text-align: center;width: 85%;}img{width: 25rem;display: inline-block;margin: 0 0 5rem;}h1{font-family: Raleway, sans-serif;font-size: 50px;font-weight: 700;text-transform: uppercase;display: inline-block;color: #000;margin: 0 0 1.5rem;}h4{font-size: 32px;font-family: Raleway, sans-serif;font-weight: 400;margin: 0;}</style></body><meta http-equiv="refresh" content="2.5;URL=viaje-a-corea.html"/>';
+	else echo'<body><link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet"><div><div><img src="https://www.medestore.mx/assets/img/logo_medestore-color.png" alt="Logo Medestore"></div><h1>Ocurrió un problema</h1><h4>Por favor, vuelve a intentar mandar tu mensaje.</h4></div><style>body{margin: 0;padding: 0;background-color: #fff;background-size: cover;height: 100vh;width: 100vw;}body>div {position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);display: block;text-align: center;width: 85%;}img{width: 25rem;display: inline-block;margin: 0 0 5rem;}h1{font-family: Raleway, sans-serif;font-size: 50px;font-weight: 700;text-transform: uppercase;display: inline-block;color: #000;margin: 0 0 1.5rem;}h4{font-size: 32px;font-family: Raleway, sans-serif;font-weight: 400;margin: 0;}</style></body><meta http-equiv="refresh" content="2.5;URL=viaje-a-corea.html"/>';
+?>
