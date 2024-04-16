3c068c378fca65bf70fb4dff2a018351030cde9e
André Vital <andre@andrevital.com>
2024-04-16 10:40:46 -0600

    Add dbConnection file tracking

---
+++ dbConnection.php
@@ -0,0 +1,10 @@
+<?php
+$hostname = "localhost";
+$username = "intracel_mluser";
+$password = "y3ll0wp4thyellow";
+$databaseName = "intracel_controlContacto";
+
+$connect = mysqli_connect($hostname, $username, $password, $databaseName);
+mysqli_set_charset($connect, "utf8");
+date_default_timezone_set('America/Mexico_City');
+?>
