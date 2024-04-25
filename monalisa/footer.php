8ad628222877b4bee6a33aea1d2bc832c17edc9d
André Vital <andre@andrevital.com>
2024-04-25 17:16:05 -0600

    Monalisa format

--- monalisa/footer.php
+++ monalisa/footer.php
@@ -1,53 +1,69 @@
-        <aside class="ms-footbar section fp-auto-height">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-lg-9 ms-footer-col">
-              <div class="ms-footbar-block">
-                <p>
-                  <a href="../aviso-de-privacidad.php">Aviso de privacidad </a>|
-                  <a href="../politicas-de-servicio-tecnico.php">Políticas de servicio técnico </a>|
-                  <a href="../politicas-de-venta.php">Políticas de venta </a>|
-                  <a href="../condiciones.php">Promociones y Restricciones</a>
-                </p>
-                
-              </div>
-              
-            </div>
-            <div class="col-lg-3 ms-footer-col ms-footer-alt-color">
-              <div class="ms-footbar-block">
-               
-                <div class="media">
-                <div class="ms-footbar-social">
-                  <div class="footer-social-links">
-                  <a href="https://www.facebook.com/medestore/" class="btn-circle btn-facebook" target="_blank">
-                    <i class="fa-brands fa-facebook-f"></i>
-                  </a>
-                  <a href="https://www.instagram.com/medestoremx/" class="btn-circle btn-instagram" target="_blank">
-                    <i class="fa-brands fa-instagram"></i>
-                  </a>
-                  <a href="https://twitter.com/Medestoremx" class="btn-circle btn-twitter" target="_blank">
-                    <i class="fa-brands fa-x-twitter"></i>
-                  </a>
-                  <a href="https://www.youtube.com/user/MedeStore" class="btn-circle btn-youtube" target="_blank">
-                    <i class="fa-brands fa-youtube"></i>
-                  </a>
-                  </div>
-                </div>
-                </div>
-              </div>
-            </div>
-            
-          </div>
-        </div>
-      </aside>
-      <footer class="ms-footer section fp-auto-height">
-        <div class="container">
-          <p>Medestore&reg;<?php echo date('Y'); ?>. Todos los derechos reservados</p>
-        </div>
-      </footer>
-      <div class="btn-back-top">
-        <a href="#" data-scroll id="back-top" class="btn-circle btn-circle-primary btn-circle-sm btn-circle-raised ">
-          <i class="zmdi zmdi-long-arrow-up"></i>
-        </a>
-      </div>
-      
+<aside class="ms-footbar section fp-auto-height">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-9 ms-footer-col">
+				<div class="ms-footbar-block">
+					<p>
+						<a href="../aviso-de-privacidad.php">Aviso de privacidad </a>|
+						<a href="../politicas-de-servicio-tecnico.php">Políticas de servicio técnico </a>|
+						<a href="../politicas-de-venta.php">Políticas de venta </a>|
+						<a href="../condiciones.php">Promociones y Restricciones</a>
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-3 ms-footer-col ms-footer-alt-color">
+				<div class="ms-footbar-block">
+					<div class="media">
+						<div class="ms-footbar-social">
+							<div class="footer-social-links">
+								<a
+									href="https://www.facebook.com/medestore/"
+									class="btn-circle btn-facebook"
+									target="_blank"
+								>
+									<i class="fa-brands fa-facebook-f"></i>
+								</a>
+								<a
+									href="https://www.instagram.com/medestoremx/"
+									class="btn-circle btn-instagram"
+									target="_blank"
+								>
+									<i class="fa-brands fa-instagram"></i>
+								</a>
+								<a
+									href="https://twitter.com/Medestoremx"
+									class="btn-circle btn-twitter"
+									target="_blank"
+								>
+									<i class="fa-brands fa-x-twitter"></i>
+								</a>
+								<a
+									href="https://www.youtube.com/user/MedeStore"
+									class="btn-circle btn-youtube"
+									target="_blank"
+								>
+									<i class="fa-brands fa-youtube"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</aside>
+<footer class="ms-footer section fp-auto-height">
+	<div class="container">
+		<p>Medestore&reg;<?php echo date('Y'); ?>. Todos los derechos reservados</p>
+	</div>
+</footer>
+<div class="btn-back-top">
+	<a
+		href="#"
+		data-scroll
+		id="back-top"
+		class="btn-circle btn-circle-primary btn-circle-sm btn-circle-raised"
+	>
+		<i class="zmdi zmdi-long-arrow-up"></i>
+	</a>
+</div>
